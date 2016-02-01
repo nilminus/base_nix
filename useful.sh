456ae0c451e4b794ee760fd387449487520a9442
Nil Minus <nilminus@gmail.com>
2016-02-01 16:13:04 +0200

    Install C helpers

--- useful.sh
+++ useful.sh
@@ -11,6 +11,11 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
+
+# Install C helpers
+sudo apt-get install -y cscope exuberant-ctags cflow
+
+# Install general stuff
 sudo apt-get install -y socat
 sudo apt-get install -y samba smbclient cifs-utils
 sudo apt-get install -y sharutils uuencode
