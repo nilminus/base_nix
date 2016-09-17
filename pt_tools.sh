1f28be6a08d1fbd1cd69658555b240f3809216a1
Nil Minus <nilminus@gmail.com>
2016-09-17 16:28:34 +0300

    Update pt_tools.sh

--- pt_tools.sh
+++ pt_tools.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 pip install virtualenv      # Not installed already?
+pip install virtualenvwrapper
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
+mkdir -p ~/tools
 
 
 # Get CrackMapExec for Pentesting Windows environments
+cd ~/tools
 git clone https://github.com/byt3bl33d3r/CrackMapExec.git
 cd CrackMapExec
-virtualenv venv
-source venv/bin/activate
+mkvirtualenv CrackMapExec
+workon CrackMapExec
 pip install --upgrade -r requirements.txt
 deactivate
 
