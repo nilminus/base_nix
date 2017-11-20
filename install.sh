425995c5fe8d0c4fa76ddb155ef550b2db80672f
Nil Minus <nilminus@gmail.com>
2017-11-20 04:01:00 +0000

    Moved to plug

--- install.sh
+++ install.sh
@@ -44,8 +44,8 @@ if $RC; then
     cp ./dotfiles/zshrc ~/.zshrc
 
     # Install vim plugins
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
+    #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    #vim +PluginInstall +qall
 fi
 
 if $CTF; then
